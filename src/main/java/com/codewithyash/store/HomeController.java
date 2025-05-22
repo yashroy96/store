aa29131a87e4e9606a5c297ad7c4c7709fafc0e2
YASH ROY (yaroy) <yashroy96@gamil.com>
2025-05-22 16:32:46 +0530

    Getting started with springboot

---
+++ src/main/java/com/codewithyash/store/HomeController.java
@@ -0,0 +1,18 @@
+package com.codewithyash.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Value("${app.myname}")
+    private String name;
+
+    @RequestMapping("/")
+    public String getHome(){
+        return "Hello World from: "+ name;
+    }
+}
